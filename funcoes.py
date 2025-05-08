3c26a09c3dd15d95b7e3ef0c24278ce68a1bf21f
Barbosamatheusilva <matheusbarbosa0272@gmail.com>
2025-05-08 15:52:42 -0300

    códigos funções
    Códigos funções fábrica de programadores string input e outros.

---
+++ funcoes.py
@@ -0,0 +1,50 @@
+texto_escrito = "Este livro é legal!"
+outro_texto = 'Vou aprender a programar.'
+outro_texto = texto_escrito
+#print (outro_texto)
+
+
+primeiro_nome = "Matheus "
+segundo_nome = "Barbosa"
+nome_completo = primeiro_nome + segundo_nome
+saudação = "Olá " + nome_completo + " tudo bem?"
+#print (saudação)
+
+
+
+balas_subtraidas = 3
+quantidade_balas = 20
+quantidade_balas -= balas_subtraidas
+#print (quantidade_balas)
+
+balas_multiplicadas = 2
+quantidadetotal_balas = 10
+quantidadetotal_balas *= balas_multiplicadas
+#print (quantidadetotal_balas)
+
+
+balas_divididas = 2
+quantidadebalas_total = 10
+quantidadebalas_total /= balas_divididas
+#print (quantidadebalas_total)
+
+
+#nome = input()
+#saudacao = "Olá " + nome
+#print(saudacao)
+
+
+
+numero_string = input()
+numero_inteiro = int(numero_string)
+subtracao = numero_inteiro - 2
+print(subtracao)
+
+
+numero_um = input()
+numero_dois = input()
+resultado_soma = int(numero_um)+int(numero_dois)
+resultado_soma_texto = str(resultado_soma)
+mensagem = "O resultado da soma é: "+resultado_soma_texto
+print(mensagem)
+
