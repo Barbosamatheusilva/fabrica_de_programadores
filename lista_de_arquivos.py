2c45dd3966833d681a28b8b96250023e1612adf1
Barbosamatheusilva <matheusbarbosa0272@gmail.com>
2025-05-09 15:56:34 -0300

    Lista de exercícios
    Realizado na aula do dia 09/05 na fábrica de programadores

---
+++ lista_de_arquivos.py
@@ -0,0 +1,19 @@
+#Ex1
+#valor = input()
+#valor_calculado = int(valor) ** 2
+#apresentação = "A área do quadrado é:" + str(valor_calculado)
+#print(apresentação)
+
+#Ex2
+#salariopreco = input("Me informe seu salario: ")
+#calculo_reajuste = 0.15 * int(salariopreco) + int(salariopreco)
+#reajuste_salario = "Seu salário foi reajustado para "+ str(calculo_reajuste)+(", e teve um reajuste de 15%")
+#print(reajuste_salario)
+
+#Ex3
+#base1 = input("Me informe a base do triângulo: ")
+#altura1 = input("Me informe a altura do triângulo: ")
+#calculoarea = int(altura1) * int(base1)/3
+#areadabase = "A área do triângulo é de: " +str(calculoarea)
+#print(areadabase)
+
